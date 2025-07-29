3fb55b63b27a71401bb79df4cf21daf9dae976a4
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-07-29 14:51:59 +0300

    routes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import blogData from "./data/blog";
+import Header from "./components/Header";
+import About from "./components/About";
+import ArticleList from "./components/ArticleList";
+import Article from "./components/Article";
+
+console.log(blogData);
+
+function App() {
+  return (
+
+    <div className="App">
+      <Header/>
+      <About/>
+      <ArticleList/>
+      <Article/>
+      
+    </div>
+  );
+}
+
+export default App;
